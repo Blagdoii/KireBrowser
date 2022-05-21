46a726dc30f6adc260d7159a6110bef8b3d60fc8
Blagdoi <73658321+Blagdoii@users.noreply.github.com>
2022-05-21 14:11:34 +0200

    Add files via upload

---
+++ if not works.bat
@@ -0,0 +1,8 @@
+start https://python.org
+start https://chromedriver.chromium.org/downloads
+echo If you installed this
+pause
+pip install selenium
+pip install requests
+pip install proxy
+taskkill cmd
